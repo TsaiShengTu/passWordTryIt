577440cfc666f4ec4ad1a84c8abda35a888ab80f
TsaiShengTu <108114468+TsaiShengTu@users.noreply.github.com>
2022-11-10 09:12:35 +0800

    Initial Commit

---
+++ passWordTryIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passWordTryIt
+//
+//  Created by Sheng-Yu on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
